ff772504c5de1585c124eaa0c50646f11f9a08c7
Valery <wrld.more@gmail.com>
2023-09-13 09:50:47 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from turtle import*
+
+left(180)
+pen()
+speed(4000)
+for i in range(180):
+		forward(1)
+		right(1)
+left(90)
+for i in range(180):
+		forward(1)
+		right(1)
+left(90)
+for i in range(180):
+	forward(1)
+	right(1)
+left(90)
+for i in range(180):
+	forward(1)
+	right(1)
+penup()
+for x in range(-5, 15):
+	for y in range(-6, 16):
+		setpos(x*11.5, y*11.5)
+		dot(3)
+done()
