5ec159b4f439167738c10e991d756d445b6c3bd4
Valery <wrld.more@gmail.com>
2023-09-23 14:52:52 +0300

    double flower

--- main.py
+++ main.py
@@ -3,24 +3,36 @@ from turtle import*
 
 pen()
 speed(4000)
-for i in range(180):
+for i in range(90):
 		forward(1)
-		right(1)
+		right(2)
 left(90)
-for i in range(180):
+for i in range(90):
 		forward(1)
-		right(1)
+		right(2)
 left(90)
-for i in range(180):
+for i in range(90):
 	forward(1)
+	right(2)
+left(90)
+for i in range(90):
+	forward(1)
+	right(2)
+left(90)
+forward(20)
+for i in range(180):
+	forward(0.5)
 	right(1)
 left(90)
 for i in range(180):
-	forward(1)
+	forward(0.5)
 	right(1)
-penup()
-for x in range(-5, 15):
-	for y in range(-6, 16):
-		setpos(x*11.5, y*11.5)
-		dot(3)
+left(90)
+for i in range(180):
+	forward(0.5)
+	right(1)
+left(90)
+for i in range(180):
+	forward(0.5)
+	right(1)
 done()
